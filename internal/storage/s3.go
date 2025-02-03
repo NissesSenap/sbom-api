0b25fd5f07f2c60ea79f04f2bfbaa12d861822d6
Edvin Norling <edvin.norling@kognic.com>
2025-02-03 17:49:45 +0100

    maybe got minio endpoint to work....

--- internal/storage/s3.go
+++ internal/storage/s3.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 )
@@ -15,13 +16,19 @@ type S3Storage struct {
 	client *s3.Client
 }
 
-func NewS3Storage() (*S3Storage, error) {
+func NewS3Storage(endpoint, region, accessKey, secretKey string) (*S3Storage, error) {
+	// https://stackoverflow.com/questions/67575681/is-aws-go-sdk-v2-integrated-with-local-minio-server
 	cfg, err := config.LoadDefaultConfig(context.TODO())
+	client := s3.NewFromConfig(cfg, func(o *s3.Options) {
+		o.Credentials = credentials.NewStaticCredentialsProvider(accessKey, secretKey, "")
+		o.BaseEndpoint = &endpoint
+		o.Region = "us-east-1"
+		o.UsePathStyle = true
+	})
 	if err != nil {
-		return nil, fmt.Errorf("unable to load SDK config: %w", err)
+		return nil, fmt.Errorf("failed to create S3 client: %w", err)
 	}
 
-	client := s3.NewFromConfig(cfg)
 	return &S3Storage{client: client}, nil
 }
 
